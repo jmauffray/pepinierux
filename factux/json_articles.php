95ab92aa0f13cd06e849da43250a21d20de9d01b
Jean-Marie Auffray <jmauffray@gmail.com>
2014-08-22 19:24:10 +0200

    add JSON pages for article, bon and client

---
+++ factux/json_articles.php
@@ -0,0 +1,58 @@
+<?php 
+require_once("include/verif.php");
+include_once("include/config/common.php");
+//0.6Mo pour 1000 articles (3300 en base)
+//$sql = "SELECT * FROM " . $tblpref ."article  LIMIT 0, 200";
+$sql = "SELECT num, article, variete, taille FROM " . $tblpref ."article";
+
+mysql_query("set names 'utf8'");
+$req = mysql_query($sql) or die('Erreur SQL !<br>'.$sql2.'<br>'.mysql_error());
+$rows = array();
+ 
+while($data = mysql_fetch_array($req))
+  {
+    $rows[] = $data;
+  }
+
+$s=json_encode($rows);
+echo $s;
+
+/*
+  echo count($rows);
+  $var=json_decode($s);
+  var_dump($var);
+  echo $var[0];
+
+  foreach($var[0] as $item) { //foreach element in $arr
+  echo "->" . $item[0]; //etc
+  }
+
+  echo "<br/>";
+
+  switch (json_last_error()) {
+  case JSON_ERROR_NONE:
+  //          echo ' - Aucune erreur';
+  break;
+  case JSON_ERROR_DEPTH:
+  echo ' - Profondeur maximale atteinte';
+  break;
+  case JSON_ERROR_STATE_MISMATCH:
+  echo ' - Inadéquation des modes ou underflow';
+  break;
+  case JSON_ERROR_CTRL_CHAR:
+  echo ' - Erreur lors du contrôle des caractères';
+  break;
+  case JSON_ERROR_SYNTAX:
+  echo ' - Erreur de syntaxe ; JSON malformé';
+  break;
+  case JSON_ERROR_UTF8:
+  echo ' - Caractères UTF-8 malformés, probablement une erreur d\'encodage';
+  break;
+  default:
+  echo ' - Erreur inconnue';
+  break;
+  }
+*/
+?>
+
+
---
+++ factux/json_clients.php
@@ -0,0 +1,59 @@
+<?php
+require_once("include/verif.php");
+include_once("include/config/common.php");
+
+//sql request
+$sql = "SELECT * FROM " . $tblpref ."client  LIMIT 0, 200";
+mysql_query("set names 'utf8'");
+$req = mysql_query($sql) or die('Erreur SQL !<br>'.$sql2.'<br>'.mysql_error());
+
+//fill array with sql results
+$rows = array(); 
+while($data = mysql_fetch_array($req))
+  {
+    $rows[] = $data;
+  }
+
+//json encode
+$s=json_encode($rows);
+echo $s;
+
+/*
+  echo count($rows);
+  $var=json_decode($s);
+  var_dump($var);
+  echo $var[0];
+
+  foreach($var[0] as $item) { //foreach element in $arr
+  echo "->" . $item[0]; //etc
+  }
+
+  echo "<br/>";
+
+  switch (json_last_error()) {
+  case JSON_ERROR_NONE:
+  //          echo ' - Aucune erreur';
+  break;
+  case JSON_ERROR_DEPTH:
+  echo ' - Profondeur maximale atteinte';
+  break;
+  case JSON_ERROR_STATE_MISMATCH:
+  echo ' - Inadéquation des modes ou underflow';
+  break;
+  case JSON_ERROR_CTRL_CHAR:
+  echo ' - Erreur lors du contrôle des caractères';
+  break;
+  case JSON_ERROR_SYNTAX:
+  echo ' - Erreur de syntaxe ; JSON malformé';
+  break;
+  case JSON_ERROR_UTF8:
+  echo ' - Caractères UTF-8 malformés, probablement une erreur d\'encodage';
+  break;
+  default:
+  echo ' - Erreur inconnue';
+  break;
+  }
+*/
+?>
+
+
