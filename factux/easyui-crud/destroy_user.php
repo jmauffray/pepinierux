e1b3e64fc94b44e8a0e1c40c3db195a5cce2a10a
breizhux <breizhux@localhost>
2015-09-13 17:31:00 +0200

    clean code

--- factux/easyui-crud/destroy_user.php
+++ factux/easyui-crud/destroy_user.php
@@ -1,11 +1,14 @@
 <?php
 
+#hack library does not sent 'num'
 $id = intval($_REQUEST['id']);
 
 include '../include/config/common.php';
 
 $sql = "update " . $tblpref ."article set actif='non' where num=$id";
+
 @mysql_query($sql);
 
 echo json_encode(array('success'=>true));
+
 ?>
