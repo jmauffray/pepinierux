77d7ae6e624c5faae5fa70102b26917a9f9d4abb
breizhux <breizhux@localhost>
2017-05-03 10:31:30 +0200

    Fix filename to export plantes

--- factux/export_articles.php
+++ factux/export_articles.php
@@ -22,7 +22,8 @@ mysql_query("set names 'utf8'");
 $req = mysql_query($sql) or die('Erreur SQL !<br>' . $sql . '<br>' . mysql_error());
 
 //output
-$filename = "plantes.csv";
+$today = date("Y-m-d-H-i-s");
+$filename = "plantes-".$today.".csv";
 $fp = fopen('php://output', 'w');
 
 //HTTP header
