a9dfcc3d262a8c8703e98d8e3f62c1606b2cab62
breizhux <breizhux@localhost>
2017-06-17 18:48:44 +0200

    Update disponible export

--- factux/export_articles.php
+++ factux/export_articles.php
@@ -13,12 +13,23 @@ include_once("include/utils.php");
 error_reporting(E_ALL);
 
 //SQL
-$columns = array('num', 'article', 'variete', 'taille', 'contenance', 'prix_ttc_part', 'prix_htva_part', 'categorie', 'stock', 'stock_disponible');
+$columns = array('num',
+    'article',
+    'variete',
+    'taille', 
+    'contenance',
+    'prix_ttc_part',
+    'prix_htva', 
+    'categorie',
+    'localisation',
+    'conditionnement',
+    'stock',
+    'stock_disponible');
 $sql = "SELECT ".implode(",", $columns)
         . " FROM " . $tblpref . "article, " . $tblpref . "categorie"
         . " WHERE actif != 'non' AND stock_disponible > 0 AND"
         . " " . $tblpref . "article.cat = " . $tblpref . "categorie.id_cat "
-        . " ORDER by article";
+        . " ORDER by article, taille";
 mysql_query("set names 'utf8'");
 $req = mysql_query($sql) or die('Erreur SQL !<br>' . $sql . '<br>' . mysql_error());
 
