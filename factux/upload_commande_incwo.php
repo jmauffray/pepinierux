fa3914e3d1738904bbd54a31ef2adcb0c7319b2a
Jean-Marie Auffray <jmauffray@gmail.com>
2023-11-19 20:27:10 +0100

    After incwo update, add 2 columns of offet to retrieve data (#9)

--- factux/upload_commande_incwo.php
+++ factux/upload_commande_incwo.php
@@ -44,20 +44,21 @@ if (($handle = fopen($target_file, "r")) !== FALSE) {
             //echo $data[$c] . "<br />\n";
         }
         
-        $article = intval($data[7]);
-        $quanti = intval($data[12]);
-        $prix_remise_ht = $data[19];
+        $offset = 2;
+        $article = intval($data[7 + $offset]);
+        $quanti = intval($data[12 + $offset]);
+        $prix_remise_ht = $data[19 + $offset];
         $prix_remise_1_art_ht = 0;
         if ($quanti > 0) {
             $prix_remise_1_art_ht = ($prix_remise_ht / $quanti);
         }
-        $tva = substr($data[13], 0, -1);
+        $tva = substr($data[13 + $offset], 0, -1);
         
         //compute price with tva
         $prix_remise_1_art_ttc = ((1 + ($tva / 100)) * $prix_remise_1_art_ht);
         
         //avoir, multiply price per -1
-        $isFacture = $data[3];
+        $isFacture = $data[3 + $offset];
         if($isFacture != "1")
         {
             $prix_remise_1_art_ttc = $prix_remise_1_art_ttc * -1;
